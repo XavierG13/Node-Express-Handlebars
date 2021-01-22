5e5e571c0f29fadccc40a3a2d82c2518e2c7027e
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-22 00:42:32 -0600

    added folders and files for project. Set up server.js to use express and express-handlebars. Set up connections.js to create connection to use in ORM.

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Set up MySql connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Creature13X!",
+  database: "",
+});
+
+// Create connection
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting" + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Exports the connection for ORM to use
+module.exports = connection;
