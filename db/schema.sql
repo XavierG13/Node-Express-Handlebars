23d7e8301800cb65e1832db910a46105c334d954
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-24 14:08:29 -0600

    fixed schema to have burger_name as not null and set devoured to default to false, added burgers to seeds.sql, fixed burger-block.handlebars where names of bugers were not displaying properly (had them under names but changed to burger_name to match schema).

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,8 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    burger_name VARCHAR(30),
-    devoured BOOLEAN
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,12 @@
 INSERT INTO burgers (burger_name)
-VALUES ('Big Mac');
+VALUES ('Spicy Chicken Burger');
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Baconator', true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Whataburger', true);
 INSERT INTO burgers (burger_name)
-VALUES ('Baconator');
+VALUES ('Avacado Burger');
 INSERT INTO burgers (burger_name)
-VALUES ('Whataburger');
+VALUES ('Veggie Burger');
