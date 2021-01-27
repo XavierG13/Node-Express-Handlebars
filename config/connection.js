546b241bfa41c30a7bff9bac2bf0af9db03ee6fb
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-26 21:25:35 -0600

    added jawsdb to app

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySql connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Creature13X!",
-  database: "burgers_db",
-});
+var connection;
+
+if (process.env.JAWDB_URL) {
+  connection = mysql.createConnection(process.env.JAWDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Creature13X!",
+    database: "burgers_db",
+  });
+}
 
 // Create connection
 connection.connect(function (err) {
